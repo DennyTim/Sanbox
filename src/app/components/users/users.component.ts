405bdfe73d2a76c8ed8a3554df3652a483eb86e2
Tim <luckystudydanit@gmail.com>
2019-08-21 08:55:11 +0300

    Added changes

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: User[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'Ma'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'Ma'
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'Fl'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Diana',
+      lastName: 'Jackson',
+      age: 44,
+      address: {
+        street: '12 Wake st',
+        city: 'Boston',
+        state: 'Ma'
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+}
