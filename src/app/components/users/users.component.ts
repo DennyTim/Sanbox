b99bf2e988e83a97777e55ee0eec5e17ff2d9dfa
Tim <luckystudydanit@gmail.com>
2019-08-22 09:15:55 +0300

    Before begin pipe`s videp

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,56 +8,80 @@ import { User } from 'src/app/models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
+  currentStyles = {};
 
   constructor() {}
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-          street: '50 Main st',
-          city: 'Boston',
-          state: 'Ma'
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'Ma'
+          },
+          image: 'http://lorempixel.com/600/600/people/3',
+          isActive: true
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'Ma'
+          },
+          image: 'http://lorempixel.com/600/600/people/2',
+          isActive: false
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'Fl'
+          },
+          image: 'http://lorempixel.com/600/600/people/1',
+          isActive: true
         }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 34,
-        address: {
-          street: '20 School st',
-          city: 'Lynn',
-          state: 'Ma'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Williams',
-        age: 26,
-        address: {
-          street: '55 Mill st',
-          city: 'Miami',
-          state: 'Fl'
-        }
-      }
-    ];
+      ];
 
-    this.addUser({
-      firstName: 'Diana',
-      lastName: 'Jackson',
-      age: 44,
-      address: {
-        street: '12 Wake st',
-        city: 'Boston',
-        state: 'Ma'
-      }
-    });
+      this.loaded = true;
+      // this.addUser({
+      //   firstName: 'Diana',
+      //   lastName: 'Jackson'
+      // });
+      this.setCurrentClasses();
+      this.setCurrentStyles();
+    }, 2000);
   }
 
   addUser(user: User) {
     this.users.push(user);
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd,
+      'big-text': this.showExtended
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px'
+    };
   }
 }
