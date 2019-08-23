0927ed90b0e83153724afd7b3a8406ed67c573fb
Tim <luckystudydanit@gmail.com>
2019-08-23 08:55:20 +0300

    Add new changes

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,77 +11,56 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
 
   constructor() {}
 
   ngOnInit() {
-    setTimeout(() => {
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 'Doe',
-          age: 30,
-          address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'Ma'
-          },
-          image: 'http://lorempixel.com/600/600/people/3',
-          isActive: true
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'Ma'
         },
-        {
-          firstName: 'Kevin',
-          lastName: 'Johnson',
-          age: 34,
-          address: {
-            street: '20 School st',
-            city: 'Lynn',
-            state: 'Ma'
-          },
-          image: 'http://lorempixel.com/600/600/people/2',
-          isActive: false
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: false
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'Ma'
         },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-            street: '55 Mill st',
-            city: 'Miami',
-            state: 'Fl'
-          },
-          image: 'http://lorempixel.com/600/600/people/1',
-          isActive: true
-        }
-      ];
+        isActive: false,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'Fl'
+        },
+        isActive: true,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      }
+    ];
 
-      this.loaded = true;
-      // this.addUser({
-      //   firstName: 'Diana',
-      //   lastName: 'Jackson'
-      // });
-      this.setCurrentClasses();
-      this.setCurrentStyles();
-    }, 2000);
+    this.loaded = true;
   }
 
   addUser(user: User) {
     this.users.push(user);
-  }
-
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended
-    };
-  }
-
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px',
-      'font-size': this.showExtended ? '' : '40px'
-    };
   }
 }
